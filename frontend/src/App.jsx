db9dca72a17cbf3b72f6254a8d059fadf85264cd
manulbp <palihenamanul4@gmail.com>
2025-03-18 18:40:05 +0530

    Bug fixing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-export const App = () => {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import AboutUs from '../pages/AboutUs';
+import ContactUs from '../pages/ContactUs';
+import Products from '../pages/Products';
+import Orders from '../pages/Orders';
+
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </Router>
+  );
 }
