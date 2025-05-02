13e3049b9cd2d54220ac9d69606221b5bc7571ee
manulbp <palihenamanul4@gmail.com>
2025-05-03 02:09:59 +0530

    Update Header.jsx

--- frontend/component/Header.jsx
+++ frontend/component/Header.jsx
@@ -26,9 +26,9 @@ export default function Header() {
             <input
               type="text"
               placeholder="Search..."
-              className="pl-10 pr-3 py-1 rounded border border-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500 text-sm"
+              className="pl-10 pr-3 py-1 rounded border border-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 text-sm"
             />
-            <div className="absolute left-2 top-1.5 text-gray-500">
+            <div className="absolute left-2 top-1.5 text-gray-700">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
               </svg>
