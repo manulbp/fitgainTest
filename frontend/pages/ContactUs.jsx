5b7cd7bb432e24aa602fe4ef8de944fa0bf61fe8
manulbp <palihenamanul4@gmail.com>
2025-05-03 13:15:03 +0530

    Update ContactUs.jsx

--- frontend/pages/ContactUs.jsx
+++ frontend/pages/ContactUs.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ContactUs() {
   return (
-    <div>ContactUs</div>
+    <div className="flex flex-col min-h-screen bg-gray-200 px-6 py-8"></div>
   )
 }
