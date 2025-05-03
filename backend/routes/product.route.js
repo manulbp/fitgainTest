c40816eece3934c19afaef8f8e9100819926938e
manulbp <palihenamanul4@gmail.com>
2025-05-04 01:09:32 +0530

    Update product.route.js

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,16 +1,21 @@
 import express from 'express';
-import { deleteProduct, test, updateProduct, addproducts , getbyId, getAllProducts } from '../controllers/product.controller.js';
-
+import { 
+  deleteProduct, 
+  test, 
+  updateProduct, 
+  addproducts, 
+  getbyId, 
+  getAllProducts,
+  uploadMiddleware 
+} from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.get('/', test);
-router.post('/add', addproducts);
+router.post('/add', uploadMiddleware, addproducts);
 router.get('/all', getAllProducts);
-router.put('/:pid', updateProduct);
+router.put('/:pid', uploadMiddleware, updateProduct);
 router.delete('/:pid', deleteProduct);
 router.get('/:pid', getbyId);
-
-
 
 export default router;
