3479361d9b7282ac879f06a606ee250bc98e263d
manulbp <palihenamanul4@gmail.com>
2025-05-03 13:15:24 +0530

    Update Orders.jsx

--- frontend/pages/Orders.jsx
+++ frontend/pages/Orders.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Orders() {
   return (
-    <div>Orders</div>
+    <div className="flex flex-col min-h-screen bg-gray-200 px-6 py-8"></div>
   )
 }
