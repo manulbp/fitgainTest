7a92703cc178ee2dbd30f83b37ac7bb0816b4048
manulbp <palihenamanul4@gmail.com>
2025-05-05 23:25:07 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import AboutUs from '../pages/AboutUs';
 import ContactUs from '../pages/ContactUs';
 import Header from '../component/Header';
 import Product from '../pages/Product';
@@ -22,7 +21,6 @@ export default function App() {
       
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
         <Route path="/product" element={<Product />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/orders" element={<Orders />} />
